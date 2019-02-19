08a3a7c55bb89e2c0605ff7f5300f10069f10b8d
ibrahemuwk <hemavic1995@gmail.com>
2019-02-19 13:38:51 +0200

    Add project files.

---
+++ WatchStore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WatchStore.Startup))]
+namespace WatchStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
